6bac44a3c5c535d7dca3140e50aaf1153ee2aec7
Varun Kumar <kumarvarunn656@gmail.com>
2026-08-14 18:23:20 +0530

    adding the bad request exception

---
+++ src/main/java/com/firstclub/membership/common/exception/BadRequestException.java
@@ -0,0 +1,16 @@
+package com.firstclub.membership.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends RuntimeException {
+
+    private final HttpStatus status = HttpStatus.BAD_REQUEST;
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
